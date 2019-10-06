9692bfd7853638e55d780b3e7e2bc62b8efc7521
harshitjindal <harshit.jindal@icloud.com>
2019-10-06 23:15:32 +0530

    Initial Commit

---
+++ lib/markAttendancePage.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class MarkAttendancePage extends StatefulWidget {
+  @override
+  _MarkAttendancePageState createState() => _MarkAttendancePageState();
+}
+
+class _MarkAttendancePageState extends State<MarkAttendancePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
